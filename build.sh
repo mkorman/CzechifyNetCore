bf8e97102a311742f77570729b0c8d2b5ad230dc
Mariano Korman <mkormanc@gmail.com>
2020-06-15 10:28:31 +0100

    Improved build script and tweaked CSS

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 # build the application
 docker build -t czechify .
+
 # run the application inside the container
-docker run -d -p 8080:80 czechify
+docker stop czechify_dev || true 
+docker rm czechify_dev || true 
+docker run -d -p 8080:80 --name czechify_dev czechify
