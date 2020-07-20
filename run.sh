1a6bdaf0888c4ed5e86005203dc0bdd689d1a207
Mariano Korman <mkormanc@gmail.com>
2020-07-20 08:42:39 +0100

    Adding a docker-compose file And removed now redundant run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-# stop any running instances
-docker stop czechify_dev || true 
-docker rm czechify_dev || true 
-# run the app mounting the "logs" volume to capture the logs locally 
-docker run -d -p 8080:80 --name czechify_dev -v //c/work/tmp://logs czechify 
